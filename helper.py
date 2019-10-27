4669c4ed97173752d41d9af63c75d69d0534a4b0
muhammad umer javed <muhammad.umer@g1g.com>
2019-10-27 15:11:30 +0500

    SD-T5 - Compute the ELA noise in the images

--- helper.py
+++ helper.py
@@ -152,3 +152,24 @@ def detect_and_color_splash(model, image_path=None):
         # Save output
         file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
         skimage.io.imsave(file_name, splash)
+        
+def convert_to_ela_image(path, quality):
+    filename = path
+    resaved_filename = filename.split('.')[0] + '.resaved.jpg'
+    ELA_filename = filename.split('.')[0] + '.ela.png'
+    
+    im = Image.open(filename).convert('RGB')
+    im.save(resaved_filename, 'JPEG', quality=quality)
+    resaved_im = Image.open(resaved_filename)
+    
+    ela_im = ImageChops.difference(im, resaved_im)
+    
+    extrema = ela_im.getextrema()
+    max_diff = max([ex[1] for ex in extrema])
+    if max_diff == 0:
+        max_diff = 1
+    scale = 255.0 / max_diff
+    
+    ela_im = ImageEnhance.Brightness(ela_im).enhance(scale)
+    
+    return ela_im
